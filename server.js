5a239c12e70981b5b07756f5e8fe7471c37d8388
Glitch (hello-express) <none>
2020-07-06 17:17:31 +0000

    🐜✨ Checkpoint ./server.js:15553712/437:16172301/122 ./.glitch-assets:16172301/444

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const path = require('path');
 
 const client = new CommandoClient({
   commandPrefix: 'h!',
-  owner: ['', '', '', ''],
+  unknownCommandResponse: false,
+  owner: ['678941697488584734', '613364629464285316', '684519096788058145', '719252298811047936'],
   disableEveryone: true
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['admin', 'Admin-Only Commands']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+    
 });
