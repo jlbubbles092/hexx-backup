fafdbdaf2227ed65e6e47d715be828afbcccdd7a
Glitch (hello-express) <none>
2020-07-07 11:52:55 +0000

    🍕👕 Checkpoint ./commands/admin/kick.js:15553712/2567 ./commands/admin/ban.js:15553712/2614

---
+++ commands/admin/ban.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class KickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ban',
+			group: 'admin',
+			memberName: 'ban',
+			description: 'Bans a member/bot. Note: Bot role needs to be higher than the member/bot.',
+		});
+	}
+
+	run(message) {
+const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member
+          .ban({
+            reason: 'They were bad!',
+          })
+          .then(() => {
+            message.reply(`Successfully banned ${user.tag}!`);
+          })
+          .catch(err => {
+            message.reply('I was unable to ban the member.');
+            console.error(err);
+          });
+      } else {
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      message.reply("You didn't mention the user to ban!");
+    }
+  }
+}
---
+++ commands/admin/kick.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class KickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kick',
+			group: 'admin',
+			memberName: 'kick',
+			description: 'Kicks a member/bot. Note: Bot role needs to be higher than the member/bot.',
+		});
+	}
+
+	run(message) {
+		const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member
+          .kick('They were bad or it was a test!')
+          .then(() => {
+            message.reply(`Successfully kicked ${user.tag}!`);
+          })
+          .catch(err => {
+            message.reply('I was unable to kick the member.');
+            console.error(err);
+          });
+      } else {
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+  }
+};
