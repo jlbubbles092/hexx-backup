0091c6ba1c0be82e66490e79c227ad0e78b82c76
Glitch (hexx) <none>
2020-07-08 14:02:23 +0000

    🚆👼 Checkpoint ./commands/admin/slowmode.js:15553712/698

---
+++ commands/admin/slowmode.js
@@ -0,0 +1,20 @@
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class KickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'slowmode',
+			group: 'admin',
+			memberName: 'slowmode',
+			description: '',
+		});
+	}
+
+	run(message) {
+    var time = message.content.split(' ').slice(1).join(' ')
+    if(!time) return message.reply('Please enter a time in seconds!')
+    message.channel.setRateLimitPerUser(time)
+    message.channel.send('Set the slowmode!')
+  }
+};
