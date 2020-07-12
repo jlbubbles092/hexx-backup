9ce7b834660bdb20960daa213c0e5eb62afe6850
Glitch (hexx) <none>
2020-07-12 14:06:43 +0000

    🐘🍭 Checkpoint ./commands/misc/about.js:15553712/933

--- commands/misc/about.js
+++ commands/misc/about.js
@@ -15,17 +15,19 @@ module.exports = class AboutCommand extends Command {
       const embedMessage = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('About')
-        .setAuthor(message.member.username, message.member.avatarURL())
+        .setAuthor(message.member.user.username, message.member.user.avatarURL())
         .setDescription('Sends information about the bot.')
-        .setThumbnail(message.member.avatarURL())
+        .setThumbnail(message.member.user.avatarURL())
         .addFields(
           { name: 'Discord.js Version:', value: '12.2.0' },
           { name: 'Discord.js-Commando Name:', value: 'discord.js/Commando'},
           { name: 'Node.js Version:', value: '12.x'},
           { name: 'Other Dependencies:', value: `discord.js,\ndiscord.js-commando,\ndiscord.js-minesweeper,\nexpress,\ngenius-lyrics,\nheroku,\npath,\nquick.db,\nrequest`},
          )
-        .setImage(`message.member.avatarURL())
+        .setImage(message.member.user.avatarURL())
         .setTimestamp()
-        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        .setFooter(message.member.user.username, message.member.user.avatarURL());
+      
+      message.channel.send(embedMessage);
     }
 };
