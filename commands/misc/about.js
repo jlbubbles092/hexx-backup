35cef9b3de15805784168fdf26dfa37e81e98c28
Glitch (hexx) <none>
2020-07-12 13:54:52 +0000

    ❄️🖼 Checkpoint ./commands/misc/about.js:15553712/3042 ./package.json:15553712/2

--- commands/misc/about.js
+++ commands/misc/about.js
@@ -7,11 +7,25 @@ module.exports = class AboutCommand extends Command {
             name: 'about',
             group: 'misc',
             memberName: 'about',
-            description: 'Sends information about this bot.',
+            description: 'Sends information about the bot.',
         });    
     }
 
     run(message) {
-      const embedMessage = Discord.MessageEmbed()
+      const embedMessage = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('About')
+        .setAuthor(message.member.username, message.member.avatarURL())
+        .setDescription('Sends information about the bot.')
+        .setThumbnail(message.member.avatarURL())
+        .addFields(
+          { name: 'Discord.js Version:', value: '12.2.0' },
+          { name: 'Discord.js-Commando Name:', value: 'discord.js/Commando'},
+          { name: 'Node.js Version:', value: '12.x'},
+          { name: 'Other Dependencies:', value: `discord.js,\ndiscord.js-commando,\ndiscord.js-minesweeper,\nexpress,\ngenius-lyrics,\nheroku,\npath,\nquick.db,\nrequest`},
+         )
+        .setImage(`message.member.avatarURL())
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
     }
 };
